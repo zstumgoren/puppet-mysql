b4d00a3b2f1246cd9eee000c5a2104499c0201ad
Serdar Tumgoren <zstumgoren@gmail.com>
2013-10-25 11:44:31 -0700

    Use default mysql port

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,6 +12,6 @@ class mysql::config {
   $executable = "${boxen::config::homebrewdir}/bin/mysqld_safe"
   $logdir     = "${boxen::config::logdir}/mysql"
   $logerror   = "${logdir}/error.log"
-  $port       = 13306
+  $port       = 3306
   $socket     = "${datadir}/socket"
 }
